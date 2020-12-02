d829a393d82f58f762a72feac62e3fa0b5829b83
lsinchiguanos <leslie.sinchiguano2015@uteq.edu.ec>
2020-12-01 21:48:16 -0500

    Final

--- app/src/main/java/com/example/practica3v/MainActivity.java
+++ app/src/main/java/com/example/practica3v/MainActivity.java
@@ -11,8 +11,14 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,11 +35,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         requestQueue = Volley.newRequestQueue(this);
         initUI();
-        stringRequest();
+        //stringRequest();
+        jsonRequest();
     }
 
     private void initUI(){
         textView = findViewById(R.id.lbJson);
+    }
+
+    private void jsonRequest() {
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        int size = response.length();
+                        for (int i = 0; i < size; i++){
+                            try {
+                                JSONObject jsonObject = new JSONObject(response.get(i).toString());
+                                String name = jsonObject.getString("name");
+                                textView.append(name + "\n");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("HttpClient", "error: " + error.toString());
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Public-Merchant-Id","84e1d0de1fbf437e9779fd6a52a9ca18");
+                return params;
+            }
+        };
     }
 
     private void stringRequest() {
