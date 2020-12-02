b56e58c35cd27ab22a5981cf94b7e7a89ae082b6
lsinchiguanos <leslie.sinchiguano2015@uteq.edu.ec>
2020-12-01 21:50:38 -0500

    Esta si

--- app/src/main/java/com/example/practica3v/MainActivity.java
+++ app/src/main/java/com/example/practica3v/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
                 return params;
             }
         };
+        requestQueue.add(request);
     }
 
     private void stringRequest() {
