6a65157f203c9cf37c8765b9fedb14d3fc6cd861
lsinchiguanos <leslie.sinchiguano2015@uteq.edu.ec>
2020-12-01 20:57:56 -0500

    Código de volley y configuración de permisos

--- app/src/main/java/com/example/practica3v/MainActivity.java
+++ app/src/main/java/com/example/practica3v/MainActivity.java
@@ -3,12 +3,61 @@ package com.example.practica3v;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    RequestQueue requestQueue;
+    private static final String url = "https://api-uat.kushkipagos.com/transfer-subscriptions/v1/bankList";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+        stringRequest();
     }
+
+    private void initUI(){
+        textView = findViewById(R.id.lbJson);
+    }
+
+    private void stringRequest() {
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("Public-Merchant-Id", "84e1d0de1fbf437e9779fd6a52a9ca18");
+                return params;
+            }
+        };
+        requestQueue.add(request);
+    }
+
 }
