f628a6def82dbddb6a9da561b424cd189cf657dd
lsinchiguanos <leslie.sinchiguano2015@uteq.edu.ec>
2020-12-01 21:33:57 -0500

    Formato Json

--- app/src/main/java/com/example/practica3v/MainActivity.java
+++ app/src/main/java/com/example/practica3v/MainActivity.java
@@ -12,6 +12,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
         initUI();
         stringRequest();
     }
@@ -41,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.e("HttpClient", "success! response: " + response.toString());
+                        textView.setText(response);
                     }
                 },
                 new Response.ErrorListener() {
